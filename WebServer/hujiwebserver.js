2827bcea559e891ad20a180a041c28d08ac5cb3f
YafimK <fkpkot@gmail.com>
2017-01-12 08:31:34 +0200

    Added basic methods

--- WebServer/hujiwebserver.js
+++ WebServer/hujiwebserver.js
@@ -3,11 +3,24 @@
  */
 
 function start(port, callback) {
+    //TODO: declare some default for callback
+    try{
+        var http = require("http");
+        var serverObj = http.createServer();
+
+
+    } catch (err) {
+        callback(err);
+    }
 
 }
 
+function use(command, middleware){
+    //TODO: default for middleware
 
+}
 
 module.exports = {
-
+    start: start,
+    use: use
 };
