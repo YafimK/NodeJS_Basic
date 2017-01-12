048a893e4f558bc695b83ac76f2877a133606808
YafimK <fkpkot@gmail.com>
2017-01-12 11:09:27 +0200

    added middleware, start - finished?

--- WebServer/hujiwebserver.js
+++ WebServer/hujiwebserver.js
@@ -17,18 +17,31 @@ function ServerObj(port) {
 
 
 function start(port, callback) {
-    //TODO: declare some default for callback
+    //TODO: declare some default for callback, callback??
+    this.callback = callback || function defCallback(err){console.error(err);};
+    this.port = port;
     try{
-        console.debug().log("starting server with port: " + port + " and callback " + callback);
-        return new ServerObj(port);
+        console.debug().log("starting server with port: " + this.port + " and callback " + this.callback);
+        return new ServerObj(this.port);
     } catch (err) {
-        callback(err);
+        this.callback(err);
     }
 
 }
 
 function use(command, middleware){
-    //TODO: default for middleware
+    //TODO: default for middleware, command?
+
+
+
+
+    return this;
+
+}
+
+function middleware(request,response,next){
+
+
 
 }
 
