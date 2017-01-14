272c4424be9fad80f30804cadcb645e68c7b6f7f
raphael <refi11991@gmail.com>
2017-01-14 18:19:33 +0200

    router update handler

--- WebServer/router.js
+++ WebServer/router.js
@@ -3,16 +3,20 @@
  */
 
 
+
+
 let router = function() {
 };
 
-router.prototype.controllerSet = new Map();
+
+router.prototype.controllerSet =  [];
+router.prototype.continueIteration = true;
 
 router.prototype.addRoute = function(path, middleWare)
 {
     //TODO: check if middleware is function with typeof
     this.path = path || "/";
-    this.controllerSet.set(path, middleWare);
+    this.controllerSet.push({path: path, middleWare: middleWare});
 };
 
 
@@ -21,15 +25,30 @@ function extractHttpMethod(method)
 
 }
 
+function next(){
+    "use strict";
+    this.continueIteration = true;
+}
+
+function checkMatch(curPath, reqCheckPath){
+    "use strict";
+    return true
+}
+
  router.prototype.httpHandler = function(req) {
-    let requestUrl = this.controllerSet.get(req.getPath());
-    if (requestUrl) {
-        console.log('Found proper action');
-        // requestUrl(request,response,'');
-    }
-    else {
-        // let response =
-    }
+     this.stopIteration = true;
+     var i = 0
+     var curPath = req.getPath()
+     while(i < this.controllerSet.length && this.continueIteration) {
+         "use strict";
+         if(checkMatch(entry.path,curPath)){
+             this.continueIteration = false;
+             entry.middleware
+         }
+     }
+
+
+
 };
 
 
