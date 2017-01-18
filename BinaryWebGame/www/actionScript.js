b8a6052bad818d100d232f6fb1b2a152e5819a18
YafimK <fkpkot@gmail.com>
2017-01-18 14:42:06 +0200

    fixed file parser for easier file parsing

--- BinaryWebGame/www/actionScript.js
+++ BinaryWebGame/www/actionScript.js
@@ -2,3 +2,27 @@
  * Created by fimka on 18/01/2017.
  */
 $ = require('https://code.jquery.com/jquery-3.1.1.js');
+
+function userChoice(num) {
+    let url = "/gamble/" + num;
+    $.ajax({url: url})  .done(function() {
+        alert( "success" );
+    })
+        .fail(function() {
+            alert( "error" );
+        })
+        .always(function() {
+            alert( "complete" );
+        });
+}
+
+function resetGame(){
+    $.send()
+}
+
+$("#user1choice").click(userChoice(1));
+
+$("#user0choice").click(userChoice(0));
+
+$("#newGame").click(resetGame());
+
