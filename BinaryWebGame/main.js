62e7c68034b19b8befaefcadf7c1bb06464eda42
YafimK <fkpkot@gmail.com>
2017-01-21 01:14:51 +0200

    Reset game support fix.

--- BinaryWebGame/main.js
+++ BinaryWebGame/main.js
@@ -14,8 +14,6 @@ let gambling = {
 
 /**
  * Button “new game” that resets the game
- Reset the number of zeros and ones users have chose so far
- You should let the server know by sending “/gamble/reset” (DELETE)
  * @param req
  * @param res
  * @param next
@@ -28,22 +26,13 @@ function resetGame(req, res, next){
 server.use('/gamble/reset', resetGame);
 
 /**
- * Button “0” means that the user choose “0”
- You should let the server know which number the user chose by sending a message to the “/gamble/0” (POST)
- You should do that using AJAX
- The server should keep counting how many users gambled on each number 0 or 1
- The server should respond with the following JSON: {“zeros”:<numberOfZeros>,”ones”:<numberOfOnes>} the client should announce if the user won/lost/tie and how many users chose each number by adding text/HTML into the screen and hide the “0” and “1” buttons.
+ *
  * @param req
  * @param res
  * @param next
  */
 function buttonClickResult(req, res, next){
-    let gambelingDict = {1: 'ones', 0: 'zeros'};
-    let currentChoice = req.params.chosenNumber;
-    let gamblingDict = {
-        1: "ones",
-        0: "zeros"
-    };
+    let gamblingDict = {1: 'ones', 0: 'zeros'};
     if(!gamblingDict.hasOwnProperty(req.params.chosenNumber)){
         next();
     }
--- BinaryWebGame/www/actionScript.js
+++ BinaryWebGame/www/actionScript.js
@@ -9,11 +9,11 @@ let gambleDict = {
 function userChoice(event) {
     let url = "/gamble/" + event.data.userNum;
 
-    $.ajax({url: url})
+    $.ajax({url: url, method: 'POST'})
         .done(function(response) {
             $('div.votingButtons').detach();
-            $('div#gameView').append($.fn.announceResult());
-        alert( "success: " + JSON.stringify(response));
+            $('div#gameView').append($.fn.announceResult(event.data.numStr, response));
+        console.log( "success: " + JSON.stringify(response));
     })
     .fail(function() {
         console.trace( "we have an issue in passing the gambling with: " );
@@ -29,10 +29,10 @@ function resetGame(){
             console.log( "success in resetting the game" );
     })
     .fail(function() {
-        console.trace( "error in resting the game: " );
+        console.warn( "error in resting the game" );
     })
     .always(function() {
-        console.log( "completed reset request" );
+        console.warn( "completed reset request" );
     });
 }
 
@@ -43,24 +43,52 @@ function resetGame(){
         $("<input id=\"user0choice\" type=\"button\" value=\"0\">").appendTo(".votingButtons");
         $("<input id=\"newGame\" type=\"button\" value=\"New game\">").appendTo(".votingButtons");
         $("#user1choice").click({userNum: 1, numStr: 'ones'}, userChoice);
-        $("#user0choice").click({userNum: 0, numStr: 'zeroes'}, userChoice);
+        $("#user0choice").click({userNum: 0, numStr: 'zeros'}, userChoice);
         $("#newGame").click(resetGame);
         return this;
     };
 })( jQuery );
 
 (function( $ ){
-    $.fn.announceResult = function(choiceResult) {
-        let result = "";
-        if("win"){
-            result = "You have won!"
-        } else if ('lose'){
-            result = "You have lost!"
+    $.fn.announceResult = function(chosenNumber, result) {
+       let choiceResult;
+
+       if( result.ones === result.zeros ){
+           choiceResult = "tie";
+       } else if((result.ones > result.zeros && chosenNumber === "ones" )
+            || (result.ones < result.zeros && chosenNumber === "zeros" )){
+            choiceResult = "win";
         } else {
-            result = "Ladies and gentleman, we've got a tie!!!"
+           choiceResult = "lose";
+       }
+
+       let annoucment = '';
+        switch(choiceResult)
+        {
+            case "win":
+                annoucment = "You have won!";
+                break;
+            case "lose":
+                annoucment = "You have lost!";
+                break;
+            case "tie":
+                annoucment = "Ladies and gentleman, we've got a tie!!!"
+                break;
+            default:
+                console.warn("something went wrong in calculating results!")
+            break;
         }
-        $("<div class='Annoucment'></div>").appendTo("#gameView");
-        $("<p></p>").appendTo(".Annoucment").html(result);
+
+        $("<div class='annoucment'></div>").appendTo("#gameView");
+        $("<p></p>").appendTo(".annoucment").html(annoucment);
+        $("<p id=\"results\"></p>").appendTo(".annoucment").html("Votes count per number");
+        $("<table class=\"resultsTable\"></table>").appendTo('#results');
+        $("<thead></thead>").appendTo(".resultsTable");
+        $("<tr></tr>").appendTo("thead").append("<th>Number</th><th>Vote count</th>");
+        $("<tbody></tbody>").appendTo(".resultsTable");
+        $("<tr class='vote0Row'></tr>").appendTo("tbody").append("<td>zeros</td><td>" + result.zeros + "</td>");
+        $("<tr class='vote1Row'></tr>").appendTo("tbody").append("<td>ones</td><td>" + result.ones + "</td>");
+
         return this;
     };
 })( jQuery );
