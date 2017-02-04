6758e426d703e07398d296d26eb43b66a4363e87
YafimK <fkpkot@gmail.com>
2017-02-04 11:19:12 +0200

    ASync file read fix

--- BinaryWebGame/main.js
+++ BinaryWebGame/main.js
@@ -2,10 +2,24 @@
  * Created by fimka on 17/01/2017.
  */
 var pathLib = require("path");
-var server = require('./../WebServer/hujiwebserver');
-var STATUS_CODES = require('./../WebServer/httpStandard').STATUS_CODES;
+var server = require('./WebServer/hujiwebserver');
+var STATUS_CODES = require('./WebServer/httpStandard').STATUS_CODES;
 
 var fs = require("fs");
+
+//Polyfill to get easy capability to endswith for getting the file extension from ES6
+if (!String.prototype.endsWith) {
+    String.prototype.endsWith = function(searchString, position) {
+        var subjectString = this.toString();
+        if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+            position = subjectString.length;
+        }
+        position -= searchString.length;
+        var lastIndex = subjectString.lastIndexOf(searchString, position);
+        return lastIndex !== -1 && lastIndex === position;
+    };
+}
+
 
 server.start(8081, function(err){
     if(err != undefined){
@@ -14,7 +28,7 @@ server.start(8081, function(err){
 
 //Store the last score
 var gambling = {
-  ones: 0,
+    ones: 0,
     zeros: 0
 };
 
@@ -68,15 +82,6 @@ function serveHttpFiles(req, res, next){
         requestedFilePath = req.path;
     }
 
-    var ext = requestedFilePath.slice(requestedFilePath.indexOf('.') + 1);
-    if(ext === 'css'){
-        res.set('content-type', 'text/css');
-    } else if(ext === 'js') {
-        res.set('content-type', 'application/javascript');
-    } else if (ext === "html") {
-        res.set('content-type', 'text/html');
-    }
-
     requestedFilePath = '/www' + requestedFilePath;
     requestedFilePath = __dirname + requestedFilePath;
     requestedFilePath = pathLib.normalize(requestedFilePath);
@@ -85,6 +90,14 @@ function serveHttpFiles(req, res, next){
         if (err) {
             res.status(500).send(STATUS_CODES[500]);
             return;
+        }
+
+        if(requestedFilePath.endsWith('css')){
+            res.set('content-type', 'text/css');
+        } else if(requestedFilePath.endsWith('js')) {
+            res.set('content-type', 'application/javascript');
+        } else if (requestedFilePath.endsWith('html')) {
+            res.set('content-type', 'text/html');
         }
 
         res.set('content-length', data.toString());
