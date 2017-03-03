345c6c179f624547f6691d580cc48ac97fdf9d6c
YafimK <fkpkot@gmail.com>
2017-03-03 19:08:00 +0200

    Added sending the Ajax

--- reactXGame/app/GameResultView.jsx
+++ reactXGame/app/GameResultView.jsx
@@ -1,59 +1,71 @@
-import React from 'react';
+import React from "react";
 
-export default class PlayResultView extends React.Component{
-    constructor(props){
+export default class GameResultView extends React.Component {
+    constructor(props) {
         super(props);
-
+        this.annonunceWinner = this.annonunceWinner.bind(this);
     }
 
-    anonunceWinner(chosenNumber, result) {
-    let choiceResult;
+    annonunceWinner(result, chosenNumber) {
+        let choiceResult;
+        console.log(chosenNumber, result);
+        if (result.ones === result.zeros) {
+            choiceResult = "tie";
+        } else if ((result.ones > result.zeros && chosenNumber === "1" )
+            || (result.ones < result.zeros && chosenNumber === "0" )) {
+            choiceResult = "lose";
+        } else {
+            choiceResult = "win";
+        }
 
-    if( result.ones === result.zeros ){
-        choiceResult = "tie";
-    } else if((result.ones > result.zeros && chosenNumber === "ones" )
-        || (result.ones < result.zeros && chosenNumber === "zeros" )){
-        choiceResult = "lose";
-    } else {
-        choiceResult = "win";
-    }
+        let announcement = '';
+        switch (choiceResult) {
+            case "win":
+                announcement = "You have won!";
+                break;
+            case "lose":
+                announcement = "You have lost!";
+                break;
+            case "tie":
+                announcement = "Ladies and gentleman, we've got a tie!!!"
+                break;
+            default:
+                // console.warn("something went wrong in calculating results!")
+                break;
+        }
 
-    let announcement = '';
-    switch(choiceResult)
-    {
-        case "win":
-            announcement = "You have won!";
-            break;
-        case "lose":
-            announcement = "You have lost!";
-            break;
-        case "tie":
-            announcement = "Ladies and gentleman, we've got a tie!!!"
-            break;
-        default:
-            // console.warn("something went wrong in calculating results!")
-            break;
-    }
-
-    return(
-            <div class='annoucment'>
-                <p id='gameResult'>
-                    <p id="results"> Votes count per number</p>
-                    <table class="resultsTable">
-                        <thead>
-                        <tr><th>Number</th><th>Vote count</th></tr>
-                        </thead>
-                        <tbody>
-                        <tr class='vote0Row'><td>zeros</td><td>{this.props.result.zeros}</td></tr>
-                        <tr class='vote1Row'><td>ones</td><td>{this.props.result.ones}</td></tr>
-                        </tbody>
-                    </table>
-                </p>
+        return ( <div className='annoucment'>
+                <p id='gameResult'> {announcement}</p>
+                <p id="results">Votes count per number</p>
+                <table className="resultsTable">
+                    <thead>
+                    <tr>
+                        <th>Number</th>
+                        <th>Vote count</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr className='vote0Row'>
+                        <td>zeros</td>
+                        <td>{result.zeros}</td>
+                    </tr>
+                    <tr className='vote1Row'>
+                        <td>ones</td>
+                        <td>{result.ones}</td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
-    );
-};
+        );
+    };
 
-    render(){
-        <div></div>
-    }
+    render() {
+        console.log(this.props);
+        self = this;
+
+        let resultDiv = this.annonunceWinner(self.props.results, self.props.chosenButton);
+        return (
+            <div>{resultDiv}</div>
+        )
+    };
 }
--- reactXGame/app/GameView.jsx
+++ reactXGame/app/GameView.jsx
@@ -6,26 +6,31 @@ import GameResultView from './GameResultView.jsx';
 export default class GameView extends React.Component{
     constructor(props){
         super(props);
-        this.state = {gameStage: 'Vote', results: {}};
+        this.state = {gameStage: 'Vote', results: {}, chosenButton: -1};
         this.onResetGameClick = this.onResetGameClick.bind(this);
         this.onVoteClick = this.onVoteClick.bind(this);
     }
-
     getInitialState(){
         return {gameStage: 'Vote'}
     }
-
     onVoteClick(event){
-        console.log(" GPressed: " + event.target.value);
-        let url = "/gamble/" + event.target.value;
-        console.log("choosen");
-
+        let chosen = event.target.value;
+        //TODO: check vote
+        console.log(" GPressed: " + chosen);
+        let url = "/gamble/" + chosen;
+        self =this;
         let xhr = new XMLHttpRequest();
         xhr.open('POST', url, true);
         xhr.onload = function(e) {
             if (this.status == 200) {
                 console.log(this.response);
-                this.setState({ gameStage: 'Results', results: this.response});
+                let response = JSON.parse(this.response);
+                console.log(response);
+
+                self.setState({ gameStage: 'Results', results: response, chosenButton: chosen});
+            } else{
+                let e = e || "";
+                console.log("We got an error receiving the gamble - " + e);
             }
         };
         xhr.send();
@@ -33,27 +38,30 @@ export default class GameView extends React.Component{
     }
     onResetGameClick(event){
         console.log("Reseting");
+        this.setState({ gameStage: 'Reset', results: {}, chosenButton: -1});
         let xhr = new XMLHttpRequest();
-        xhr.open('DELETE', "/gamble/reset", true);
+        xhr.open("DELETE", "/gamble/reset", true);
         xhr.onload = function(e) {
             if (this.status == 200) {
                 console.log(this.response);
-                this.setState({ gameStage: 'Reset' });
+                console.log("reseting");
+                this.setState({ gameStage: 'Reset', results: {}, chosenButton: -1});
             }
         };
+        xhr.send();
     }
-
 
     render(){
         let currentStage = null;
         if(this.state.gameStage === 'Vote'){
             currentStage = <VoteMenu onVoteFunc={this.onVoteClick.bind(this)}/>
         } else if (this.state.gameStage === 'Results'){
-            <GameResultView/>
+            currentStage = <GameResultView results = {this.state.results} chosenButton = {this.state.chosenButton}/>
         }
         return (<div>
             {currentStage}
             <GameMenu onResetFunc={this.onResetGameClick} />
-        </div>);
+        </div>
+        );
     }
 }
